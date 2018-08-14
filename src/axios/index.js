bbb5015496c6570ec0937dc3df8bf78826b69131
lq <844388470@qq.com>
2018-08-14 19:38:41 +0800

    Add:添加axios库

---
+++ src/axios/index.js
@@ -0,0 +1,24 @@
+// import axios from 'axios'
+// import { message } from 'antd';
+
+
+// axios.defaults.baseURL = process.env.host
+// axios.defaults.timeout = 10000
+// axios.defaults.withCredentials = true
+
+// axios.interceptors.request.use((config)=>{
+//     return config;
+// },(err)=>{
+//     message.error(`${err}`)
+//     return Promise.reject(err);
+// })
+
+// axios.interceptors.response.use((res)=>{
+//     return res;
+// },(err)=>{
+//     message.error(`${err}`)
+//     return Promise.reject(err);
+// })
+  
+
+// export default axios
